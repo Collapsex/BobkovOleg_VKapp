78667ce91e210f2f9df220dcb697bdcaa76f1ff1
Collapsex <32868612+Collapsex@users.noreply.github.com>
2017-10-20 16:34:20 +0300

    Добавил необходимые UITabbarViewController, UITableViewController и UICollectionViewController в нужном порядке. Под вопросом пока что остается переход только при верно введенном логине и пароле.

---
+++ BobkovOleg_VKapp/TabAfterLoginController.swift
@@ -0,0 +1,35 @@
+//
+//  TabAfterLoginController.swift
+//  BobkovOleg_VKapp
+//
+//  Created by Олег Бобков on 20.10.17.
+//  Copyright © 2017 Олег Бобков. All rights reserved.
+//
+
+import UIKit
+
+class TabAfterLoginController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BobkovOleg_VKapp/ViewController.swift
+++ BobkovOleg_VKapp/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
+enum LoginError:Error {
+    case wrongUsername
+    case wrongPassword
+    case wrongUsernameAndPassword
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    private let succesfullLoginName = "Oleg"
+    private let successfullPassword = "123456"
+
+    override var preferredStatusBarStyle: UIStatusBarStyle { return  .default }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+
+
+    @IBAction func buttonBeenTouched(_ sender: UIButton) {
+
+
+    guard let username = loginTextField.text else {
+    print("login text field is empty")
+    return
+    }
+
+    guard let password = passwordTextField.text else {
+    print("Password text field is empty")
+    return
+    }
+
+    let result = loginWith(username: username, password: password)
+
+    if result.success {
+    print("Successfull login")
+    } else {
+    print("Failed login")
+    if let error = result.error {
+    switch error {
+    case .wrongUsernameAndPassword : print("Username and password are wrong")
+    case .wrongUsername : print("Username is wrong")
+    case .wrongPassword : print("Password is wrong")
+
+    }
+    }
+        }
+    }
+    private func loginWith(username: String, password: String) -> (success: Bool,error: LoginError?)  {
+
+        if username == succesfullLoginName && password == successfullPassword {
+            return (true,nil)
+        }
+
+        if username == succesfullLoginName {
+            return(false,.wrongPassword)
+        }
+
+        if password == successfullPassword {
+            return (false,.wrongUsername)
+        }
+
+        return (false, .wrongUsernameAndPassword)
+
+    }
 
 }
 
