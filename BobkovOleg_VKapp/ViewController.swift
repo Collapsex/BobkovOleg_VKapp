eb30c9c30c6955fd855416f285a75df4db2cf651
Collapsex <32868612+Collapsex@users.noreply.github.com>
2017-10-20 14:58:27 +0300

    Initial Commit

---
+++ BobkovOleg_VKapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BobkovOleg_VKapp
+//
+//  Created by Олег Бобков on 20.10.17.
+//  Copyright © 2017 Олег Бобков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
